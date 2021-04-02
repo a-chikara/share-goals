5c078448eda6ee211e40c852ea53f059f4175d48
a-chikara <aiba102chikara@gmail.com>
2021-04-02 15:16:58 +0900

    メッセージ送信機能の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,25 @@
 class PostsController < ApplicationController
   def index
+    @post = Post.new
+    @room = Room.find(params[:room_id])
   end
+
+  def create
+    @room = Room.find(params[:room_id])
+    @post = @room.posts.new(post_params)
+    if @post.save
+      redirect_to room_posts_path(@room)
+    else
+      render :index
+    end
+  end
+
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content).merge(user_id: current_user.id)
+  end
+
 
 end
