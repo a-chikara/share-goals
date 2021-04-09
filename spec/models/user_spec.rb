f5d7d20da62244c5260bf51cac3496a00183a302
a-chikara <aiba102chikara@gmail.com>
2021-04-09 12:26:42 +0900

    ユーザー新規登録についての単体テスト

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,16 +1,63 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe '#create' do
-    before do
-      @user = FactoryBot.build(:user)
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe 'ユーザー新規登録' do
+    context '登録できる場合' do
+      it '全て正しく入力できていれば新規登録できる' do
+        expect(@user).to be_valid
+      end
+
+      it 'passwordが6文字以上であれば登録できること' do
+        @user.password = "123456"
+        @user.password_confirmation = "123456"
+        expect(@user).to be_valid
+      end  
     end
 
-    it 'nameが空では登録できないこと' do
-      @user.name = ''
-      @user.valid?
-      # expect(@user.errors.full_messages).to include("Name can't be blank")
-      binding.pry
+
+    context '登録できない場合' do
+      it 'nameが空では登録できないこと' do
+        @user.name = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Nameを入力してください")
+      end
+
+      it 'emailが空では登録できないこと' do
+        @user.email = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Eメールを入力してください")
+      end
+
+      it 'passwordが空では登録できないこと' do
+        @user.password = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("パスワードを入力してください")
+      end
+
+      it 'passwordが5文字以下であれば登録できないこと' do
+        @user.password = "12345"
+        @user.password_confirmation = '12345'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("パスワードは6文字以上で入力してください")
+      end
+
+      it 'passwordとpassword_confirmationが不一致では登録できないこと' do
+        @user.password = '123456'
+        @user.password_confirmation = '1234567'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("パスワード（確認用）とパスワードの入力が一致しません")
+      end
+
+      it '重複したemailが存在する場合登録できないこと' do
+        @user.save
+        another_user = FactoryBot.build(:user, email: @user.email)
+        another_user.valid?
+        expect(another_user.errors.full_messages).to include("Eメールはすでに存在します")
+      end
     end
   end
 end
