d6a31563085adfa14cf30d41f59939be1cd03c26
a-chikara <aiba102chikara@gmail.com>
2021-04-09 15:17:30 +0900

    新規投稿機能の単体テスト

--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -6,16 +6,18 @@ RSpec.describe Room, type: :model do
   end
   describe 'ユーザー新規登録' do
     context '登録できる場合' do
-
       it 'nameの値が存在すれば登録できること' do
         expect(@room).to be_valid
       end
+    end
 
+    context '登録できない場合' do
       it 'nameが空では登録できないこと' do
         @room.name = ""
         @room.valid?
         expect(@room.errors.full_messages).to include("Nameを入力してください")
       end
     end
+    
   end
 end
