ff1102286a7c4b685c5ed5b426bc52f72027fe5a
a-chikara <aiba102chikara@gmail.com>
2021-04-23 18:27:57 +0900

    選択肢の編集

--- app/models/category.rb
+++ app/models/category.rb
@@ -6,7 +6,9 @@ class Category < ActiveHash::Base
     { id: 3, name: '美容' },
     { id: 4, name: '食べ物' },
     { id: 5, name: '欲しいもの' },
-    { id: 6, name: 'その他' },
+    { id: 6, name: 'アクティビティ' },
+    { id: 7, name: 'ライフスタイル' },
+    { id: 8, name: 'その他' },
   ]
 
   include ActiveHash::Associations
--- app/models/deadline.rb
+++ app/models/deadline.rb
@@ -1,15 +1,18 @@
 class Deadline < ActiveHash::Base
   self.data = [
     { id: 1, name: '---' },
-    { id: 2, name: '1週間' },
-    { id: 3, name: '1ヶ月' },
-    { id: 4, name: '3ヶ月' },
-    { id: 5, name: '6ヶ月' },
-    { id: 6, name: '1年' },
-    { id: 7, name: '3年' },
-    { id: 8, name: '5年' },
-    { id: 9, name: '10年' },
-    { id: 10, name: 'その他' },
+    { id: 2, name: '1ヶ月以内' },
+    { id: 3, name: '3ヶ月以内' },
+    { id: 4, name: '6ヶ月以内' },
+    { id: 5, name: '1年以内' },
+    { id: 6, name: '3年以内' },
+    { id: 7, name: '5年以内' },
+    { id: 8, name: '10年以内' },
+    { id: 9, name: '20年以内' },
+    { id: 10, name: '30年以内' },
+    { id: 11, name: '40年以内' },
+    { id: 12, name: '50年以内' },
+    { id: 13, name: '目標期日なし' },
   ]
 
   include ActiveHash::Associations
