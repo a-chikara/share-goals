e0eb2086a059b540f3c6ef072055040af5f23303
a-chikara <aiba102chikara@gmail.com>
2021-04-23 17:25:02 +0900

    マイグレーションファイルの編集

--- db/migrate/20210402045214_create_posts.rb
+++ db/migrate/20210402045214_create_posts.rb
@@ -1,10 +1,10 @@
 class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
-      t.string  :title
-      t.text    :memo
-      t.string  :category
-      t.string  :deadline
+      t.string     :title
+      t.text       :memo
+      t.integer    :category_id
+      t.integer    :deadline_id
       t.references :room, foreign_key: true
       t.references :user, foreign_key: true
       t.timestamps
